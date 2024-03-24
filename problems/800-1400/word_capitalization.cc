559cbd474a322cbbb55c984c428ff5b3409ddcc0
Aaron <aaronwang.wxy@gmail.com>
2024-03-24 10:02:52 -0400

    word capitalization

---
+++ problems/800-1400/word_capitalization.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vt vector
+
+void solve() {
+    string s;
+    cin >> s;
+    s[0] = toupper(s[0]);
+    cout << s << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
