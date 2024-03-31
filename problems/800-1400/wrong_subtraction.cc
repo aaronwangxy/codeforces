9242d54454e129020b096d5f9193dc656ad63566
Aaron <aaronwang.wxy@gmail.com>
2024-03-31 14:30:30 -0400

    wrong subtraction

---
+++ problems/800-1400/wrong_subtraction.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vt vector
+
+void solve() {
+    int n,k;
+    cin >> n >> k;
+    for (int i = 0; i < k; ++i) {
+        if (n%10) {
+            n -= 1;
+        } else {
+            n /= 10;
+        }
+    }
+    cout << n << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
