4fc9da5174ef6eadaf465a98421b51b84fed8626
Aaron <aaronwang.wxy@gmail.com>
2024-04-16 10:18:41 -0400

    gravity flip

---
+++ problems/800-1400/gravity_flip.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vt vector
+
+void solve() {
+    int n;
+    cin >> n;
+    vt<int> a(n);
+    for (auto& v:a) cin >> v;
+
+    sort(a.begin(), a.end());
+    for (auto v:a) cout << v << " ";
+    cout << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
